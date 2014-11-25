700b2dd7d5f1fb29ba0d36aec73dd5e5ad082d14
Eric Kryski <e.kryski@gmail.com>
2014-11-25 10:00:33 -0700

    changing to use the new text component interface

--- chai-react.js
+++ chai-react.js
@@ -173,7 +173,7 @@
 
     var foundMatch = false;
     for (var i = 0; i < textComponents.length; i++) {
-      if (textComponents[i].props === text || textComponents[i].props.text === text || textComponents[i].props.children === text) {
+      if (textComponents[i].props === text || textComponents[i].props.children === text) {
         flag(this, 'object', textComponents[i]);
         foundMatch = true;
         break;
